3c2893ebbd68f4ecd054f079862893a1c24d7aee
jairxortega <104592853+jairxortega@users.noreply.github.com>
2023-12-11 14:11:55 -0600

    Initial Commit

---
+++ FarmApp/FarmAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarmAppApp.swift
+//  FarmApp
+//
+//  Created by Jair Ortega on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FarmAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
